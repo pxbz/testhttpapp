4ab187ad7c8df8a69164a3865abc98c13f96425a
BuildTools <unconfigured@null.spigotmc.org>
2023-11-01 23:19:45 +0000

    Commit from vs code

---
+++ scripts.js
@@ -0,0 +1,7 @@
+
+let clickCounter = 0;
+document.getElementById("test").setAttribute("onclick", "aaa()");
+
+function aaa() {
+  document.getElementById("test").innerHTML = `Click counter: ${++clickCounter}`;
+}
