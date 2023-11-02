e6dd92e1dc7752c18a2b93aea0559644d58efd28
BuildTools <unconfigured@null.spigotmc.org>
2023-11-02 00:22:34 +0000

    moved backend code to backend.js

--- scripts.js
+++ scripts.js
@@ -1,37 +1,3 @@
-const express = require('express');
-const app = express()
-const PORT = 8080;
-
-app.use(express.json())
-
-app.get('/tshirt', (req, res) => {
-  res.status(200).send({
-    tshirt: "red",
-    size: "large"
-  })
-});
-
-app.post('/tshirt:id', (req, res) => {
-
-  const { id } = req.params;
-  const { logo } = req.body;
-  
-  if (!logo) {
-    res.status(418).send({ message: 'We need a logo!' })
-  }
-  else {
-    res.send({tshirt: `red with your ${logo} and ID of ${id}`})
-  }
-});
-
-app.get('/echo', (req, res) => {
-  res.send(req)
-})
-
-app.listen()
-
-
-
 let clickCounter = 0;
 document.getElementById("test").setAttribute("onclick", "aaa()");
 
