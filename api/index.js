0dad82c0469745c1fd3f57b7698ef20734fdc344
BuildTools <unconfigured@null.spigotmc.org>
2023-11-02 00:51:00 +0000

    using index.js now because apparently i have to

--- api/index.js
+++ api/index.js
@@ -1,4 +1,35 @@
-// const app = require('express')();
+const express = require('express');
+const app = express()
+
+// app.use(express.json())
+
+app.get('/tshirt', (req, res) => {
+  res.status(200).send({
+    tshirt: "red",
+    size: "large"
+  })
+});
+
+app.post('/tshirt:id', (req, res) => {
+
+  const { id } = req.params;
+  const { logo } = req.body;
+  
+  if (!logo) {
+    res.status(418).send({ message: 'We need a logo!' })
+  }
+  else {
+    res.send({tshirt: `red with your ${logo} and ID of ${id}`})
+  }
+});
+
+app.post('/echo', (req, res) => {
+  res.send(req)
+})
+
+app.get('/ping', (req, res) => {
+  res.send({ping: "pong!"})
+})
 
 // app.get('/api', (req, res) => {
 //   const path = `/api/item/v5}`;
@@ -12,4 +43,4 @@
 //   res.end(`Item: ${slug}`);
 // });
 
-// module.exports = app;
+module.exports = app
