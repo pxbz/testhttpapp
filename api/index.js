6989480c4dab6830aee0a713aed9626116724ea9
BuildTools <unconfigured@null.spigotmc.org>
2023-11-02 00:46:17 +0000

    fix bug

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 
 app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
+  const path = `/api/item/v5}`;
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
