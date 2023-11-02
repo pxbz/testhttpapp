5002a88efc5d4708305d63957ea30577fed9ae88
BuildTools <unconfigured@null.spigotmc.org>
2023-11-02 00:56:30 +0000

    test

--- api/index.js
+++ api/index.js
@@ -13,6 +13,20 @@ app.get('/api', (req, res) => {
   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
 
+app.get('/api/ping', (req, res) => {
+  const path = `/api/item/v5}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get('/ping', (req, res) => {
+  const path = `/api/item/v5}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
 app.get('/api/item/:slug', (req, res) => {
   const { slug } = req.params;
   res.end(`Item: ${slug}`);
