e283a2987eef684264297070947b69a7cbf63111
BuildTools <unconfigured@null.spigotmc.org>
2023-11-02 01:05:55 +0000

    test

--- api/index.js
+++ api/index.js
@@ -1,10 +1,21 @@
-const app = require('express')();
+const express = require('express')
+const app = express();
+
+app.use(express.json())
 
 // app.get('/api/ping', (req, res) => {
 //   res.setHeader('Content-Type', 'text/html');
 //   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
 //   res.end({ping: "pong"});
 // })
+
+app.post('/echo', (req, res) => {
+  res.send({test:"weeee", request: req.body});
+});
+
+app.post('/api/echo', (req, res) => {
+  res.send({test:"weeee", request: req.body});
+});
 
 app.post('/api', (req, res) => {
   res.send({test:"weeee", request: req.body});
