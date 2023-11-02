01b84eb36bddfa86e8ab7f4cb16dbbb1bc336508
BuildTools <unconfigured@null.spigotmc.org>
2023-11-02 00:49:06 +0000

    i dont know how to make a ping

---
+++ api/backend.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express()
+
+// app.use(express.json())
+
+app.get('/tshirt', (req, res) => {
+  res.status(200).send({
+    tshirt: "red",
+    size: "large"
+  })
+});
+
+app.post('/tshirt:id', (req, res) => {
+
+  const { id } = req.params;
+  const { logo } = req.body;
+  
+  if (!logo) {
+    res.status(418).send({ message: 'We need a logo!' })
+  }
+  else {
+    res.send({tshirt: `red with your ${logo} and ID of ${id}`})
+  }
+});
+
+app.post('/echo', (req, res) => {
+  res.send(req)
+})
+
+app.get('/ping', (req, res) => {
+  res.send({ping: "pong!"})
+})
+
+app.listen()
