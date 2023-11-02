a6f7868fca8fbe4be9af2417dd517f8f5bfe6982
BuildTools <unconfigured@null.spigotmc.org>
2023-11-02 00:54:53 +0000

    bug fix

--- api/index.js
+++ api/index.js
@@ -1,48 +1,21 @@
-const express = require('express');
-const app = express()
+const app = require('express')();
 
-// app.use(express.json())
-
-app.get('/tshirt', (req, res) => {
-  res.status(200).send({
-    tshirt: "red",
-    size: "large"
-  })
-});
-
-app.post('/tshirt:id', (req, res) => {
-
-  const { id } = req.params;
-  const { logo } = req.body;
-  
-  if (!logo) {
-    res.status(418).send({ message: 'We need a logo!' })
-  }
-  else {
-    res.send({tshirt: `red with your ${logo} and ID of ${id}`})
-  }
-});
-
-app.post('/echo', (req, res) => {
-  res.send(req)
-})
-
-app.get('/api/ping', (req, res) => {
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end({ping: "pong"});
-})
-
-// app.get('/api', (req, res) => {
-//   const path = `/api/item/v5}`;
+// app.get('/api/ping', (req, res) => {
 //   res.setHeader('Content-Type', 'text/html');
 //   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-//   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-// });
+//   res.end({ping: "pong"});
+// })
 
-// app.get('/api/item/:slug', (req, res) => {
-//   const { slug } = req.params;
-//   res.end(`Item: ${slug}`);
-// });
+app.get('/api', (req, res) => {
+  const path = `/api/item/v5}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
 
 module.exports = app
