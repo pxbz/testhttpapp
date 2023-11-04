9e019e97f9981f0bfae43d7ffa831f08258af9d2
BuildTools <unconfigured@null.spigotmc.org>
2023-11-04 02:34:07 +0000

    newtest

--- api/index.js
+++ api/index.js
@@ -17,23 +17,54 @@ await sql`CREATE TABLE IF NOT EXISTS test_Estimates (
 );`
 
 app.post('/api/ccc/estimate', xmlparser({trim: false, explicitArray: false}), (req, res) => {
-  let roNumber = searchEstimateXML(req.body, "/DocumentInfo/ReferenceInfo/RepairOrderID")
+  let roNumber = searchEstimateXML(req.body, "/DocumentInfo/ReferenceInfo/RepairOrderID").toString()
+  roNumber = roNumber.substring(1, roNumber.length)
   let estimatorName = searchEstimateXML(req.body, "/AdminInfo/Estimator/Party/PersonInfo/PersonName/FirstName") + " " + searchEstimateXML(req.body, "/AdminInfo/Estimator/Party/PersonInfo/PersonName/LastName")
+  let checkedInDate = searchEstimateXML(req.body, "/EventInfo/RepairEvent/ArrivalDateTime")
+  let bodyRepairsCompletionDate = searchEstimateXML(req.body, "/EventInfo/RepairEvent/ActualCompletionDateTime")
+  let pickupCompletionDate = searchEstimateXML(req.body, "/EventInfo/RepairEvent/ActualPickupDateTime")
+  let totalLossIndicator = searchEstimateXML(req.body, "/ClaimInfo/LossInfo/TotalLossInd")
+  let carColor = searchEstimateXML(req.body, "/VehicleInfo/Paint/Exterior/Color/ColorName")
+  let carMake = searchEstimateXML(req.body, "/VehicleInfo/VehicleDesc/MakeDesc")
+  let carModel = searchEstimateXML(req.body, "/VehicleInfo/VehicleDesc/ModelName") + " " + searchEstimateXML(req.body, "/VehicleInfo/VehicleDesc/ModelYear")
+  
+  let out = {
+    "RO": roNumber,
+    "Estimator": estimatorName,
+    "Checked In Date": checkedInDate,
+    "Repairs Completion Date (body repairs completion date)": bodyRepairsCompletionDate,
+    "Pickup Date": pickupCompletionDate,
+    "Total loss indicator": totalLossIndicator,
+    "Car exterior color": carColor,
+    "Car make description": carMake,
+    "Car model and year": carModel
+  }
+
+  let abc = {
+    "RO": 1,
+    "Estimator": 1,
+    "Checked In Date": 1,
+    "Repairs Completion Date (body repairs completion date)": 1,
+    "Pickup Date": 1,
+    "Total loss indicator": 1,
+    "Car exterior color": 1,
+    "Car make description": 1,
+    "Car model and year": 1
+  }
+
+  post(abc)
+  res.send(out);
+  return
 
   // let roNumber = req.body.vehicledamageestimateaddrq.documentinfo.referenceinfo.repairorderid
   // let estimatorName = req.body.vehicledamageestimateaddrq.admininfo.estimator.party.personinfo.personname.firstname + " " + req.body.vehicledamageestimateaddrq.admininfo.estimator.party.personinfo.personname.lastname
-  
-  post({"RONumber": roNumber, "Estimator": estimatorName})
-  res.send({"RO": roNumber, "Estimator": estimatorName});
-  return
-
-  let checkedInDate = req.body.vehicledamageestimateaddrq.eventinfo.repairevent.arrivaldatetime
-  let bodyRepairsCompletionDate = req.body.vehicledamageestimateaddrq.eventinfo.repairevent.actualcompletiondatetime
-  let pickupCompletionDate = req.body.vehicledamageestimateaddrq.eventinfo.repairevent.actualpickupdatetime
-  let totalLossIndicator = req.body.vehicledamageestimateaddrq.claiminfo.lossinfo.totallossind
-  let carColor = req.body.vehicledamageestimateaddrq.vehicleinfo.paint.exterior.color.colorname
-  let carMake = req.body.vehicledamageestimateaddrq.vehicleinfo.vehicledesc.makedesc
-  let carModel = req.body.vehicledamageestimateaddrq.vehicleinfo.vehicledesc.modelname + " " + req.body.vehicledamageestimateaddrq.vehicleinfo.vehicledesc.modelyear
+  // let checkedInDate = req.body.vehicledamageestimateaddrq.eventinfo.repairevent.arrivaldatetime
+  // let bodyRepairsCompletionDate = req.body.vehicledamageestimateaddrq.eventinfo.repairevent.actualcompletiondatetime
+  // let pickupCompletionDate = req.body.vehicledamageestimateaddrq.eventinfo.repairevent.actualpickupdatetime
+  // let totalLossIndicator = req.body.vehicledamageestimateaddrq.claiminfo.lossinfo.totallossind
+  // let carColor = req.body.vehicledamageestimateaddrq.vehicleinfo.paint.exterior.color.colorname
+  // let carMake = req.body.vehicledamageestimateaddrq.vehicleinfo.vehicledesc.makedesc
+  // let carModel = req.body.vehicledamageestimateaddrq.vehicleinfo.vehicledesc.modelname + " " + req.body.vehicledamageestimateaddrq.vehicleinfo.vehicledesc.modelyear
 
   // await sql`INSERT INTO test_Estimates (ro_number, estimator_full_name)
   // VALUES (${roNumber}, ${estimatorName})
